6c9df521684d2ebaa0a4085d3bf945368b25acf4
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-18 03:56:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def long_planeteer_calls(short_words)# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese|
