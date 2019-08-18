394d1fd4fa88579be8062088d7b0163fc9eb4413
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-18 03:38:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(veggies) # code an argument here
   veggies.collect do |call |
     call.capitalize + "!"
   end 
-  return extra_array
+  
 end
 
 def long_planeteer_calls# code an argument here
