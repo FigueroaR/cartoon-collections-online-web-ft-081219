bf9c809c4752da8de2ce2e6bbffafd8e45b65e76
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-18 03:35:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
+
+def roll_call_dwarves(dwarves)# code an argument here
+    dwarves.each_with_index do | name , index |
+      puts "#{index + 1 } #{name}"
+    
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer) # code an argument here
+  extra_array= []
+  veggies.collect do |call |
 end
 
 def long_planeteer_calls# code an argument here
